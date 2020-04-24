aefe46246e335d5c8574a5b72b0a102f3c010633
iskryzhytskyi <a.iskryzhytskyi@gmail.com>
2020-04-24 15:03:26 +0300

    test: partial test of cars-actions.service

--- api/src/cars/services/cars-actions.service.spec.ts
+++ api/src/cars/services/cars-actions.service.spec.ts
@@ -1,18 +1,65 @@
-import { Test, TestingModule } from '@nestjs/testing'
 import { CarsActionsService } from './cars-actions.service'
+import { Repository } from 'typeorm'
 
-describe('CarsService', () => {
+describe('CarsActionService', () => {
+  const ownersRepoMock: Repository<any> = {} as any
+  const carsRepoMock: Repository<any> = {} as any
+  let carRepoQueryBuilder
+  let ownerRepoQueryBuilder
   let service: CarsActionsService
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CarsActionsService],
-    }).compile()
+  beforeEach(() => {
+    carRepoQueryBuilder = {
+      update: jest.fn().mockReturnThis(),
+      where: jest.fn().mockReturnThis(),
+      andWhere: jest.fn().mockReturnThis(),
+      set: jest.fn().mockReturnThis(),
+      execute: jest.fn().mockResolvedValue(null),
+    } as any
 
-    service = module.get<CarsActionsService>(CarsActionsService)
+    ownerRepoQueryBuilder = {
+      delete: jest.fn().mockReturnThis(),
+      where: jest.fn().mockReturnThis(),
+      execute: jest.fn().mockResolvedValue(null),
+    } as any
+
+    carsRepoMock.createQueryBuilder = jest.fn().mockReturnValue(carRepoQueryBuilder)
+    ownersRepoMock.createQueryBuilder = jest.fn().mockReturnValue(ownerRepoQueryBuilder)
+
+    service = new CarsActionsService(carsRepoMock, ownersRepoMock)
   })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined()
+  describe('#removeOwnersByPurchaseDateLTMonthsAgo', () => {
+    it('should remove car owners which have the purchaseDate older than now - monthsCount', async () => {
+      await service.removeOwnersByPurchaseDateLTMonthsAgo(1)
+
+      expect(ownerRepoQueryBuilder.delete).toBeCalled()
+      expect(ownerRepoQueryBuilder.where).toBeCalled()
+      expect(ownerRepoQueryBuilder.execute).toBeCalled()
+    })
+
+    it.skip('should validate input', async () => {
+      // TODO
+    })
+
+    it.skip('should calculate date', async () => {
+      // TODO
+    })
   })
+
+  describe('#applyDiscountForCarsRegisteredBetweenMonthsAgo', () => {
+
+    it.skip('should calculate dates', async () => {
+      // TODO
+    })
+
+    it.skip('should calculate discount', async () => {
+      // TODO
+    })
+
+    it.skip('should validate input', async () => {
+      // TODO
+    })
+  })
+
 })
